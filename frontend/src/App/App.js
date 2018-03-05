f8e5052ac47b89b5853d765655dc17ecbe5358fe
Aquib Baig <https://github.com/GOOFY3>
2018-03-05 08:58:13 +0530

    success

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import HttpService from '../services/http-service';
 import Product from '../product/product';
+import WishList from '../wishlist/wishlist';
+
 
 const http = new HttpService();
 
@@ -31,7 +33,7 @@ class App extends Component {
     productList = () => {
       const list = this.state.products.map((product) =>
         <div className="col-sm-4" key={product._id}>
-          <Product title = {product.title} price={product.price} imgUrl={product.imgUrl}/>
+          <Product product={product}/>
         </div>
       );
 
@@ -45,9 +47,16 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to eShopping!</h1>
         </header>
-        <div className="container App-main">
+        <div className="container-fluid App-main">
           <div className = "row">
-            {this.productList()}
+            <div className="col-sm-8">
+              <div className="row">
+              {this.productList()}
+              </div>
+            </div>
+            <div className="col-sm-4">
+            <WishList/ >
+            </div>
           </div>
         </div>
       </div>
